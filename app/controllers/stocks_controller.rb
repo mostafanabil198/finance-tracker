d21cffd9af2d57b4b307f596555d8e01e9286b45
mostafa <mostafanabil198@gmail.com>
2019-04-08 16:38:05 +0200

    search stocks

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,16 @@
+class StocksController < ApplicationController
+  def search
+    if(params[:stock].present?)
+      @stock = Stock.getStockData(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:danger] = "You have entered wrong ticker name"
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:danger] = "You have entered an empty search string"
+      redirect_to my_portfolio_path
+    end
+  end
+end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,13 @@
 class Stock < ApplicationRecord
+
+  def self.getStockData(ticker_symbol)
+    begin
+      stockData = StockQuote::Stock.quote(ticker_symbol)
+      new(name: stockData.company_name, ticker: stockData.symbol, last_price: stockData.latest_price)
+    rescue Exception => e
+      return nil
+    end
+  end
+
+
 end
