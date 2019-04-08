fee1c4edbb87b41ab1651e28363e8990833170a2
mostafa <mostafanabil198@gmail.com>
2019-04-08 18:04:32 +0200

    finish search stocks

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,16 +1,13 @@
 class StocksController < ApplicationController
   def search
-    if(params[:stock].present?)
-      @stock = Stock.getStockData(params[:stock])
-      if @stock
-        render 'users/my_portfolio'
-      else
-        flash[:danger] = "You have entered wrong ticker name"
-        redirect_to my_portfolio_path
-      end
+    if(params[:stock].blank?)
+      flash.now[:danger] = "You have entered an empty search string"
     else
-      flash[:danger] = "You have entered an empty search string"
-      redirect_to my_portfolio_path
+      @stock = Stock.getStockData(params[:stock])
+      flash.now[:danger] = "You have entered wrong ticker name" unless @stock
+    end
+    respond_to do |format|
+      format.js {render partial: 'users/results'}
     end
   end
 end
